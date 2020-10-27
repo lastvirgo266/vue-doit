cf80292c7403efb26b800573caf14bf9674230ae
LaVi <no>
2020-10-27 10:29:05 +0900

    로컬 스토리지를 이용해서 데이터 가져옴(정적)

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,19 +1,32 @@
+<!-- 그런데 이런식으로 처리하면 정적으로 처리됨 -> 즉각반응 X -->
+
 <template>
   <section>
     <ul>
-      <li>할일 1</li>
-      <li>할일 2</li>
-      <li>할일 3</li>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
     </ul>
-
-
   </section>
 
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  name: "TodoList",
+
+  data(){
+    return{
+      todoItems:[]
+    }
+  },
+
+  created() {
+    if (localStorage.length > 0){
+      for(var i = 0; i<localStorage.length; i++){
+        this.todoItems.push(localStorage.key((i)));
+      }
+    }
+  },
+
 }
 </script>
 
