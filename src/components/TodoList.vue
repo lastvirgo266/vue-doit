63d07ab9b1de5d2e0fd2b99c543e981ac212266a
LaVi <no>
2020-10-27 11:04:15 +0900

    script 함수를 이용한 여러가지 동작 학습

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,7 +3,13 @@
 <template>
   <section>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+      <li v-for="(todoItem,index) in todoItems" v-bind:key="todoItem" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        {{todoItem}}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="fa fa-trash-o" aria-hidden="true"></i>
+        </span>
+      </li>
     </ul>
   </section>
 
@@ -19,6 +25,13 @@ export default {
     }
   },
 
+  methods: {
+    removeTodo(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+    }
+  },
+
   created() {
     if (localStorage.length > 0){
       for(var i = 0; i<localStorage.length; i++){
@@ -27,9 +40,39 @@ export default {
     }
   },
 
+
+
 }
 </script>
 
 <style scoped>
+  ul{
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
 
+  li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0  0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+
+
+  .checkBtn{
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+
+  .removeBtn{
+    margin-left: auto;
+    color: #de4343;
+  }
 </style>
